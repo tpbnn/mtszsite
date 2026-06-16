39c2b6dc10a66457bfa88e22f6764f9b9ee3fb68
tpbnn <hyungjoonjoo@gmail.com>
2026-06-17 02:23:21 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 콘솔 > 프로젝트 설정 > 웹 앱에서 firebaseConfig 값을 복사해 아래를 교체하세요.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmfZ7iUNnbhD_nPLjVX6il7HdS_943bEQ",
+  authDomain: "calm-mstz.firebaseapp.com",
+  databaseURL: "https://calm-mstz-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "calm-mstz",
+  storageBucket: "calm-mstz.firebasestorage.app",
+  messagingSenderId: "991864103102",
+  appId: "1:991864103102:web:f1bbb14d76c5ce5de1beef"
+};
